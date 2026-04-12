798aac2ff315800f2ffd84beac70bde23f94dcf6
“Rishikeshn2007” <rishikeshn663@gmail.com>
2026-04-12 09:09:50 +0530

    Designed a mongo DB model which will be used to add, update, remove and allocate the ambulances also added the function which will be used to connect the backend server with the mongo db atlas database at index.js amd backend/models/ambulance.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const mongoose=require('mongoose');
 require('dotenv').config();
 const app=express();
 
@@ -6,6 +7,14 @@ app.use('/',(req,res)=>{
     res.send('Home page');
 });
 
-app.listen(process.env.PORT | 3000,(req,res)=>{
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log('Successfully connected to database');
+    app.listen(process.env.PORT | 3000,(req,res)=>{
     console.log('Server running on 127.0.0.1:3000');
+    }); 
+})
+.catch((err)=>{
+    console.log('Unable to connect to the database');
 });
+
