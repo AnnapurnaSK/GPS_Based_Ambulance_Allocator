c3ede3154243f81aeab22e3d2f8bb3bb2a5442d1
Rishikeshn2007 <rishikeshn663@gmail.com>
2026-04-13 19:33:48 +0530

    Added server routes for admin, drivers and users

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const express=require('express');
 const mongoose=require('mongoose');
 require('dotenv').config();
+
+//System files
+const user_routs=require('./backend/routers/user_routs');
+const admin_routes=require('./backend/routers/admin_routs');
+const driver_routes=require('./backend/routers/driver_routs');
+
 const app=express();
 
+app.use(express.json());
+
+app.use('/user',user_routs);
+app.use('/admin',admin_routes);
+app.use('/driver',driver_routes);
+
 app.use('/',(req,res)=>{
-    res.send('Home page');
+    res.json(
+        {
+            "res":"All set"
+        }
+    )
 });
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
-    console.log('Successfully connected to database');
-    app.listen(process.env.PORT | 3000,(req,res)=>{
-    console.log('Server running on 127.0.0.1:3000');
-    }); 
+    console.log('Connected to mongo db');
+    app.listen(process.env.PORT,(req,res)=>{
+        console.log('Server running at 127.0.0.1:3000');
+    });
 })
 .catch((err)=>{
+    console.log('Unable to connect to database!');
     console.log(err);
-    console.log('Unable to connect to the database');
 });
-
