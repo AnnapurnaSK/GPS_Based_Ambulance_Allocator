7e8a752de76e87855556b097afd93418a0db4e41
Rishikeshn2007 <rishikeshn663@gmail.com>
2026-04-13 20:44:08 +0530

    Created api end points for admin to add, delete, see, search and update the status of all drivers and ambulances from the data base

--- backend/routers/admin_routs.js
+++ backend/routers/admin_routs.js
@@ -1,6 +1,14 @@
 const express=require('express');
 const router=express.Router();
 
+//SYstem files
+const admin_api=require('../api/admin_api');
+
+router.get('/api/getAllDrivers',admin_api.getAllDrivers);
+router.get('/api/getDriverByVehicleNumber/:vehicleNumber',admin_api.getDriverByVehicleNumber);
+router.post('/api/insertDriver',admin_api.insertDriver);
+router.delete('/api/deleteDriver/:vehicleNumber',admin_api.deleteDriver);
+router.put('/api/updateDriver/:vehicleNumber',admin_api.updateDriverLocationAndStatus);
 router.get('/',(req,res)=>{
     res.send('Admin default route');
 });
